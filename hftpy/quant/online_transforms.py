ddc3e3fe6cda8c66686d3f5e0ea3a69a076f011d
zozoheir <zozoheir@umich.edu>
2024-09-23 18:30:48 -0400

    updated online transforms

--- hftpy/quant/online_transforms.py
+++ hftpy/quant/online_transforms.py
@@ -1,67 +1,68 @@
 from abc import abstractmethod
 
 import numpy as np
-import pandas as pd
 
 
 class OnlineTransform:
-    def __init__(self, input_feature_name, alpha, init_value=None, adjust=True):
+    def __init__(self, input_feature_name, alpha, init_value=None, adjust=True, required_n_warmup=0):
         self.input_feature_name = input_feature_name
         self.alpha = alpha
         self.init_value = init_value
         self.adjust = adjust
         self.n = 0
+        self.required_n_warmup = required_n_warmup
+        self.value = None
 
     @abstractmethod
     def update(self, new_value):
         pass
 
     def apply(self, data):
-        std_values = []
-        for value in data:
-            self.update(value)
-            std_values.append(self.value)
+        std_values = np.zeros(len(data))
+        for i in range(len(data)):
+            self.update(data[i])
+            std_values[i] = self.value
         return std_values
 
-
+    @property
+    def warm_value(self):
+        if self.n >= self.required_n_warmup:
+            return self.value
+        else:
+            return None
 
 class ExponentialMA(OnlineTransform):
-    def __init__(self, input_feature_name, alpha, init_value=None, adjust=True):
-        super().__init__(input_feature_name, alpha, init_value, adjust)
-        self.alpha = alpha
-        self.adjust = adjust
-        if init_value is not None:
-            self.numerator = init_value * 1.0  # Assuming initial weight is 1.0
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        if kwargs.get('init_value') is not None:
+            self.numerator = kwargs['init_value'] * 1.0  # Assuming initial weight is 1.0
             self.denominator = 1.0
-            self.value = init_value
+            self.value = kwargs['init_value']
         else:
             self.numerator = 0.0
             self.denominator = 0.0
             self.value = None
 
     def update(self, value):
+        self.n += 1
         if not self.adjust:
-            # Standard EMA without adjustment
             if self.value is None:
                 self.value = value
             else:
                 self.value = self.alpha * value + (1 - self.alpha) * self.value
         else:
-            # Adjusted EMA
             self.numerator = self.alpha * value + (1 - self.alpha) * self.numerator
             self.denominator = self.alpha + (1 - self.alpha) * self.denominator
             self.value = self.numerator / self.denominator
 
 
 class ExponentialSTD(OnlineTransform):
-    def __init__(self, input_feature_name, alpha, init_value=None, adjust=True):
-        super().__init__(input_feature_name, alpha, init_value, adjust)
-        self.n = 0
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.s = 0.0
         self.s2 = 0.0
         self.W = 0.0
         self.W2 = 0.0
-        self.value = None
 
     def update(self, value):
         self.n += 1
@@ -94,35 +95,47 @@ class ExponentialSTD(OnlineTransform):
         self.value = np.sqrt(v) if self.W > 0 else np.nan
 
 
-class ExponentialZScore(OnlineTransform):
-    def __init__(self, input_feature_name, alpha, init_value=None, adjust=True):
-        super().__init__(input_feature_name, alpha, init_value, adjust)
-        self.ema = ExponentialMA(input_feature_name, alpha, init_value, adjust)
-        self.std = ExponentialSTD(input_feature_name, alpha, init_value, adjust)
-        self.value = None
+
+from collections import deque
+import numpy as np
+
+class MovingAverage(OnlineTransform):
+    def __init__(self,
+                 window_size,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.window_size = window_size
+        self.window = deque(maxlen=self.window_size)
 
     def update(self, value):
-        self.ema.update(value)
-        self.std.update(value)
-        if self.std.value is None or self.std.value == 0:
-            self.value = 0  # Define z-score as 0 if std is zero or undefined to avoid division by zero
+        self.window.append(value)
+        self.value = np.mean(self.window)
+
+    @property
+    def warm_value(self):
+        if len(self.window) == self.window_size:
+            return self.value
         else:
-            self.value = (value - self.ema.value) / self.std.value
+            return None
 
-
-class ExponentialZScoreSTLT(OnlineTransform):
-    def __init__(self, input_feature_name, st_alpha, lt_alpha, init_value=None, adjust=True):
-        super().__init__(input_feature_name, None, init_value, adjust)
-        self.ema_short = ExponentialMA(input_feature_name, st_alpha, init_value, adjust)
-        self.ema_long = ExponentialMA(input_feature_name, lt_alpha, init_value, adjust)
-        self.std_long = ExponentialSTD(input_feature_name, lt_alpha, init_value, adjust)
-        self.value = None
+class MovingStd(OnlineTransform):
+    def __init__(self,
+                 window_size,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.window_size = window_size
+        self.window = deque(maxlen=self.window_size)
 
     def update(self, value):
-        self.ema_short.update(value)
-        self.ema_long.update(value)
-        self.std_long.update(value)
-        if self.std_long.value is None or self.std_long.value == 0:
-            self.value = 0  # Define z-score as 0 if std is zero or undefined to avoid division by zero
+        self.window.append(value)
+        if len(self.window) > 1:
+            self.value = np.std(self.window, ddof=1)  # ddof=1 for sample standard deviation
         else:
-            self.value = (self.ema_short.value - self.ema_long.value) / self.std_long.value
+            self.value = 0.0
+
+    @property
+    def warm_value(self):
+        if len(self.window) == self.window_size:
+            return self.value
+        else:
+            return None
